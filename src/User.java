f397a5590a8fe887923e2ff40537e1924753cc50
CaptainCaramel <litesonicgreengrump@gmail.com>
2025-05-05 23:08:22 +0400

    Admin system started

--- src/User.java
+++ src/User.java
@@ -1,3 +1,4 @@
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
@@ -7,8 +8,10 @@ public class User implements Serializable {
     private int id;
     private String login;
     private String email;
+    private boolean isBanned;
 
-    ArrayList<Email> receivedEmails;
+    @Serial
+    private final static long serialVersionUID = 7032;
 
     User(int id, String login, String email){
         setId(id);
@@ -86,5 +89,25 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public boolean isBanned() {
+        return isBanned;
+    }
+
+    public void setBanned(boolean banned) {
+        isBanned = banned;
+    }
+
+    public boolean isAdmin(){
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "User " + login + " :" +
+                "\nUser ID : " + id +
+                "\nEmail : " + email +
+                "\nIsBanned : " + isBanned;
     }
 }
